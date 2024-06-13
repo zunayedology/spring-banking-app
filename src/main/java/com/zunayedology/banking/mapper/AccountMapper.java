3c158b320a37c2c947fbb33ccfc86fbb2d65c99a
Zunayed <zunayedology@gmail.com>
2024-06-13 11:37:44 +0600

    App updated

--- src/main/java/com/zunayedology/banking/mapper/AccountMapper.java
+++ src/main/java/com/zunayedology/banking/mapper/AccountMapper.java
@@ -6,20 +6,18 @@ import com.zunayedology.banking.entity.Account;
 public class AccountMapper {
 
     public static Account mapToAccount(AccountDto accountDto) {
-        Account account = new Account(
+        return new Account(
                 accountDto.getId(),
                 accountDto.getAccountHolderName(),
                 accountDto.getBalance()
         );
-        return account;
     }
 
     public static AccountDto mapToAccountDto(Account account) {
-        AccountDto accountDto = new AccountDto(
+        return new AccountDto(
                 account.getId(),
                 account.getAccountHolderName(),
                 account.getBalance()
         );
-        return accountDto;
     }
 }
