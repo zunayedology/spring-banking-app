5b8bf9dcb21431f728539633c5f8afc8e1aa9cf5
Zunayed <zunayedology@gmail.com>
2024-06-27 10:54:25 +0600

    Web Config updated and Credentials turned false

--- src/main/java/com/zunayedology/banking/config/WebConfig.java
+++ src/main/java/com/zunayedology/banking/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
